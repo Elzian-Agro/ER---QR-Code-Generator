65ae330a5f6dfc7b1153e0bac33662265f295c75
Suresh_Gobi <sureshgobi34@gmail.com>
2023-08-27 14:34:06 +0530

    QR UI changes

--- src/pages/qrGenerator.js
+++ src/pages/qrGenerator.js
@@ -3,6 +3,7 @@ import { useAuth } from "../AuthContext";
 import { useNavigate, Navigate } from "react-router-dom";
 import ".././assets/styles/main.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Card, Button } from "react-bootstrap";
 import {
   faUpload,
   faEye,
@@ -24,70 +25,118 @@ const QrGenerator = () => {
     localStorage.removeItem("rememberedUsername");
     localStorage.removeItem("rememberedPassword");
 
-
-
-    navigate("/login"); 
+    navigate("/login");
   };
 
   return (
     <section id="qrfinder" className="py-5">
       <div className="container">
-        <div className="row">
-          <div className="col-lg-7">
-            <h2 className="display-4 mb-4">Generate QR CODE</h2>
+        <div className="row card">
+          <div className="">
+            <h2 className="">
+              Generate QR CODE{" "}
+              <span className="">
+                {" "}
+                <button className="btn btn-danger" onClick={handleLogout}>
+                  Logout
+                </button>
+              </span>
+            </h2>
+
             <hr />
             <p>
               Excel URL:
               <input
-                  className="form-control"
-                  placeholder="Enter Excel Sheet URL"
-                ></input>
-            </p><br/>
+                className="form-control"
+                placeholder="Enter Excel Sheet URL"
+              ></input>
+            </p>
+            <br />
             <p>Upload your details </p>
             <div>
-
-
               <form>
                 <label for="fileInput">Upload file XLS:&nbsp; </label>
                 <input type="file" name="fileInput" id="fileInput" />
                 <input type="submit" className="btn-btn" value="Upload" />
+                <button className="btn-btn view-btn">
+                  <FontAwesomeIcon icon={faEye} />
+                  <a
+                    href="https://shorturl.at/lGY23"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View
+                  </a>
+                </button>
               </form>
-
-              <button className="btn-btn">
-                <FontAwesomeIcon icon={faEye} />
-                <a
-                  href="https://shorturl.at/lGY23"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View
-                </a>
-              </button>
             </div>
             <br></br>
-            <button className="btn-btn">
-              <FontAwesomeIcon icon={faSync} />
-              Generate QR Code
-            </button>
-            <button className="btn btn-danger" onClick={handleLogout}>
-              
-              Logout
-            </button>
+            <div className="gen-btn">
+              <button className="btn-btn">
+                <FontAwesomeIcon icon={faSync} />
+                Generate QR Code
+              </button>
+            </div>
           </div>
+        </div>
 
-          <div className="col-lg-5">
-            <img src={qr} alt="Contact Us" className="img-fluid" />
-            <br />
-            <button className="btn-btn">
-              <FontAwesomeIcon icon={faDownload} />
-              Download
-            </button>
-            <select id="dropdown">
-              <option value="PNG">PNG</option>
-              <option value="Pdf">Pdf</option>
-            </select>
+        <div className="qr-gen">
+          <h2>Your QR Codes</h2>
+          <hr />
+        </div>
+
+        <div className="row">
+          {/* Card 1 */}
+          <div className="col-md-4">
+            <Card style={{ width: "19rem" }} className="card">
+              <Card.Img variant="top" src={qr} />
+              <Card.Body>
+                <Card.Title>Tree ID : #712638</Card.Title>
+                <Button className="btn-btn">
+                  <FontAwesomeIcon icon={faDownload} />
+                  Download
+                </Button>
+                <select id="dropdown">
+                  <option value="PNG">PNG</option>
+                  <option value="Pdf">Pdf</option>
+                </select>
+              </Card.Body>
+            </Card>
           </div>
-          <div></div>
+          {/* Card 2 */}
+          <div className="col-md-4">
+            <Card style={{ width: "19rem" }} className="card">
+              <Card.Img variant="top" src={qr} />
+              <Card.Body>
+                <Card.Title>Tree ID : #712638</Card.Title>
+                <Button className="btn-btn">
+                  <FontAwesomeIcon icon={faDownload} />
+                  Download
+                </Button>
+                <select id="dropdown">
+                  <option value="PNG">PNG</option>
+                  <option value="Pdf">Pdf</option>
+                </select>
+              </Card.Body>
+            </Card>
+          </div>
+          {/* Card 3 */}
+          <div className="col-md-4">
+            <Card style={{ width: "19rem" }} className="card">
+              <Card.Img variant="top" src={qr} />
+              <Card.Body>
+                <Card.Title>Tree ID : #712638</Card.Title>
+                <Button className="btn-btn">
+                  <FontAwesomeIcon icon={faDownload} />
+                  Download
+                </Button>
+                <select id="dropdown">
+                  <option value="PNG">PNG</option>
+                  <option value="Pdf">Pdf</option>
+                </select>
+              </Card.Body>
+            </Card>
+          </div>
         </div>
       </div>
     </section>
