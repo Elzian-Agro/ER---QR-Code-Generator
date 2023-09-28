fa745c6bc0b6f64e9a6d1d7f93222850e749a3b3
Suresh_Gobi <sureshgobi34@gmail.com>
2023-09-28 16:56:28 +0530

    Certification Chart function and Caching the QR code

--- src/pages/qrGenerator.js
+++ src/pages/qrGenerator.js
@@ -4,8 +4,10 @@ import { useNavigate, Navigate } from "react-router-dom";
 import ".././assets/styles/main.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card, Button } from "react-bootstrap";
+import logo from "../assets/logo.png";
 import { Modal, Button as ModalButton } from "react-bootstrap";
-
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 import {
   faUpload,
   faEye,
@@ -13,7 +15,6 @@ import {
   faSync,
 } from "@fortawesome/free-solid-svg-icons";
 import "../assets/fontAwesome/fontAwesomeIcon";
-import qr from "../assets/sampleqr.jpg";
 import { utils, read } from "xlsx";
 import { useState, useEffect } from "react";
 import QRCode from "qrcode.react";
@@ -33,6 +34,31 @@ const QrGenerator = () => {
     localStorage.removeItem("rememberedPassword");
 
     navigate("/login");
+  };
+
+  const generateQRCodeDataForSelectedRow = () => {
+    if (selectedRow !== null && excelData[selectedRow]) {
+      const rowData = excelData[selectedRow];
+      const qrCodeData = `https://example.com/certificate/${rowData.Ref_No}`; // Replace with your actual URL
+      return qrCodeData;
+    }
+    return ""; // Return an empty string if there's no selected row
+  };
+
+  const generatePDF = () => {
+    if (selectedRow !== null && excelData[selectedRow]) {
+      const rowData = excelData[selectedRow];
+      const uniqueId = Date.now(); // Generate a unique ID (you can customize this logic)
+
+      const certificateElement = document.getElementById("certificate");
+
+      html2canvas(certificateElement).then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jsPDF("l", "mm", "a4");
+        pdf.addImage(imgData, "PNG", 0, 0, 297, 210); // A4 size: 210mm x 297mm
+        pdf.save(`certificate_${uniqueId}.pdf`); // Use the unique ID as the filename
+      });
+    }
   };
 
   const [excelData, setExcelData] = useState([]);
@@ -98,6 +124,53 @@ const QrGenerator = () => {
 
   const renderChart = () => {
     const ctx = document.getElementById("chart").getContext("2d");
+    const cty = document.getElementById("chartProduction").getContext("2d");
+
+    new Chart(cty, {
+      type: "line",
+      data: {
+        labels: ["Year 1", "Year 2", "Year 3", "Year 4"],
+        datasets: [
+          {
+            label: "O2 Production (Liters)",
+            data: [
+              excelData[selectedRow].O2_Production_Liters_1_years,
+              excelData[selectedRow].O2_Production_Liters_2_years,
+              excelData[selectedRow].O2_Production_Liters_3_years,
+              excelData[selectedRow].O2_Production_Liters_4_years,
+            ],
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 2,
+            pointBackgroundColor: "rgba(75, 192, 192, 1)",
+            pointRadius: 5,
+          },
+          {
+            label: "CO2 Production (Grams of C)",
+            data: [
+              excelData[selectedRow]
+                .Dynamic_Carbon_Capturing_Grams_of_C_1_years,
+              excelData[selectedRow]
+                .Dynamic_Carbon_Capturing_Grams_of_C_2_years,
+              excelData[selectedRow]
+                .Dynamic_Carbon_Capturing_Grams_of_C_3_years,
+              excelData[selectedRow]
+                .Dynamic_Carbon_Capturing_Grams_of_C_4_years,
+            ],
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 2,
+            pointBackgroundColor: "rgba(255, 99, 132, 1)",
+            pointRadius: 5,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
 
     new Chart(ctx, {
       type: "line", // Change the chart type to "line"
@@ -330,7 +403,7 @@ const QrGenerator = () => {
           {selectedRow !== null && excelData[selectedRow] && (
             <>
               <div>
-                <p>{excelData[selectedRow].Ref_No}</p>
+                {/* <p>{excelData[selectedRow].Ref_No}</p>
 
                 {excelData[selectedRow].Registration_No}
                 {excelData[selectedRow].LF_UNIT_NO}
@@ -364,17 +437,24 @@ const QrGenerator = () => {
                     .Dynamic_Carbon_Capturing_Grams_of_C_4_years
                 }
                 {excelData[selectedRow].O2_Production_Liters_4_years}
-                {excelData[selectedRow].H2O_Production_Liters_4_years}
+                {excelData[selectedRow].H2O_Production_Liters_4_years} */}
               </div>
+
               <div id="certificate">
                 <div className="container">
                   <div className="row">
                     <div className="col-md-9">
                       {/* Left side with QR code and logo */}
+                      <div className="text-center">
+                        <h4>QR Code:</h4>
+                        {selectedRow !== null && excelData[selectedRow] && (
+                          <QRCode value={generateQRCodeDataForSelectedRow()} />
+                        )}
+                      </div>
 
                       <div className="col-md-6">
-                        {/* <img src={qr} alt="QR Code" width="200" height="200" />
-    <img src={logo} alt="Logo" width="200" height="150" /> */}
+                        {/* <img src={qr} alt="QR Code" width="200" height="200" /> */}
+                        <img src={logo} alt="Logo" width="200" height="150" />
                       </div>
                       <div className="col-sm" style={{ paddingLeft: "400px" }}>
                         {/* <img src={certi}></img> */}
@@ -384,6 +464,7 @@ const QrGenerator = () => {
                         {/* <img src={chart}></img> */}
                         <div className="chart-container">
                           <canvas id="chart"></canvas>
+                          <canvas id="chartProduction"></canvas>
                         </div>
                       </div>
                     </div>
@@ -419,6 +500,11 @@ const QrGenerator = () => {
                     </div>
                   </div>
                 </div>
+              </div>
+              <div className="download-btn">
+                <button onClick={generatePDF} className="button-44">
+                  Download PDF
+                </button>
               </div>
             </>
           )}
